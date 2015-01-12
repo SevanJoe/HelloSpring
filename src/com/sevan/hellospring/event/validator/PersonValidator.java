26cbeb20f8c1737d0c849cbb7e9ded54fa6f3ae8
Sevan Joe <sevancr7@gmail.com>
2015-01-12 16:01:37 +0800

    add validator

---
+++ src/com/sevan/hellospring/event/validator/PersonValidator.java
@@ -0,0 +1,25 @@
+package com.sevan.hellospring.event.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class PersonValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object object, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "name", "name empty");
+        Person person = (Person) object;
+        if (person.getAge() < 0) {
+            errors.rejectValue("age", "illegal age value");
+        } else if (person.getAge() > 120) {
+            errors.rejectValue("age", "r u sure this man's alive?");
+        }
+    }
+
+}
